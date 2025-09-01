7ce050cabea6036c800a763f271efecf48c05005
Aleksandr Konovalov <aleksandr_konovalov@epam.com>
2025-09-01 16:27:29 +0300

    [lexical][lexical-clipboard][lexical-playground][lexical-react][lexical-selection][lexical-table][lexical-utils] Add Shadow DOM support

--- packages/lexical-playground/src/Editor.tsx
+++ packages/lexical-playground/src/Editor.tsx
@@ -84,6 +84,7 @@ import TwitterPlugin from './plugins/TwitterPlugin';
 import {VersionsPlugin} from './plugins/VersionsPlugin';
 import YouTubePlugin from './plugins/YouTubePlugin';
 import ContentEditable from './ui/ContentEditable';
+import ShadowDOMWrapper from './ui/ShadowDOMWrapper';
 
 const COLLAB_DOC_ID = 'main';
 
@@ -107,6 +108,7 @@ export default function Editor(): JSX.Element {
       hasFitNestedTables,
       isCharLimitUtf8,
       isRichText,
+      isShadowDOM,
       showTreeView,
       showTableOfContents,
       shouldUseLexicalContextMenu,
@@ -172,7 +174,9 @@ export default function Editor(): JSX.Element {
           setIsLinkEditMode={setIsLinkEditMode}
         />
       )}
-      <div
+      <ShadowDOMWrapper
+        key={`shadow-${isShadowDOM}`}
+        enabled={isShadowDOM}
         className={`editor-container ${showTreeView ? 'tree-view' : ''} ${
           !isRichText ? 'plain-text' : ''
         }`}>
@@ -311,7 +315,7 @@ export default function Editor(): JSX.Element {
           shouldPreserveNewLinesInMarkdown={shouldPreserveNewLinesInMarkdown}
           useCollabV2={useCollabV2}
         />
-      </div>
+      </ShadowDOMWrapper>
       {showTreeView && <TreeViewPlugin />}
     </>
   );
--- packages/lexical-playground/src/Settings.tsx
+++ packages/lexical-playground/src/Settings.tsx
@@ -30,6 +30,7 @@ export default function Settings(): JSX.Element {
       isCharLimit,
       isCharLimitUtf8,
       isAutocomplete,
+      isShadowDOM,
       showTreeView,
       showNestedEditorTreeView,
       // disableBeforeInput,
@@ -154,6 +155,11 @@ export default function Settings(): JSX.Element {
             onClick={() => setOption('isAutocomplete', !isAutocomplete)}
             checked={isAutocomplete}
             text="Autocomplete"
+          />
+          <Switch
+            onClick={() => setOption('isShadowDOM', !isShadowDOM)}
+            checked={isShadowDOM}
+            text="Shadow DOM"
           />
           {/* <Switch
             onClick={() => {
